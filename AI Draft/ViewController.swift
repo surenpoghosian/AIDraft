91a5c5fbbffd75cf89f2700aeb8163645ac3f8dd
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-07-01 17:36:50 +0400

    Initial Commit

---
+++ AI Draft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI Draft
+//
+//  Created by Suren Poghosyan on 01.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
